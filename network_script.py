04fee7941a3d4b5d4997c9ed487ccff29d08cfaa
macaktom <tom.macak@email.cz>
2020-05-17 21:33:21 +0200

    NewTopology - first commit

--- network_script.py
+++ network_script.py
@@ -1,2 +1,16 @@
-import nornir
+import datetime
+from nornir import InitNornir
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import netmiko_send_command
 
+
+def main():
+    nr = InitNornir(config_file="config.yml", dry_run=True)
+    results = nr.run(task=netmiko_send_command, command_string="ping 10.0.0.1", use_timing=False)
+    print_result(results)
+
+
+if __name__ == "__main__":
+    start = datetime.datetime.now()
+    main()
+    print("Time elapsed: " + str((datetime.datetime.now() - start)))
