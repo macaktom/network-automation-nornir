c5d6bb1f86285cb1386d21a3898d495e3f89c1d8
Tomas Macak <thifferx@gmail.com>
2020-09-06 22:36:27 +0200

    Added template structure and inventory files

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
-# network_automation
+# network_automation_nornir
 
 Automatická konfigurace síťových zařízení (DP práce). Pro základní příklady se podívejte na můj projekt https://gitlab.com/macaktom/nornir_dp.
--- network_script.py
+++ network_script.py
@@ -1,21 +1,24 @@
 import datetime
 from nornir import InitNornir
 from nornir.core.filter import F
-from nornir.plugins.functions.text import print_result, print_title
-from nornir.plugins.tasks.networking import napalm_get
-from nornir.plugins.tasks.networking  import napalm_ping
-from nornir.plugins.tasks.networking  import napalm_cli
+from nornir.plugins.functions.text import print_result
+from nornir.plugins.tasks.networking import napalm_cli, napalm_get
+from nornir.plugins.tasks.networking import napalm_ping
 
 
 def main():
-    nr = InitNornir(config_file="config.yml", dry_run=True)
-    cisco = nr.filter(F(groups__contains='cisco_group'))
-    junos = nr.filter(F(groups__contains='junos_group'))
-    commands = [f"ping 192.168.122.{last_octet}" for last_octet in range(1, 10)]
-    results1 = cisco.run(task=napalm_cli, commands=commands)
-    results2 = junos.run(task=napalm_ping, dest="192.168.122.1")
-    print_result(results1)
-    print_result(results2)
+    nr = InitNornir(config_file="config.yml")
+    # cisco = nr.filter(F(groups__contains='cisco_group'))
+    # junos = nr.filter(F(groups__contains='junos_group'))
+    # commands = [f"ping 192.168.122.{last_octet}" for last_octet in range(1, 10)]
+    # results1 = cisco.run(task=napalm_cli, commands=commands)
+    # results2 = junos.run(task=napalm_ping, dest="192.168.122.1")
+    # print_result(results1)
+    # print_result(results2)
+
+    cisco = nr.filter(F(groups__contains='cisco_group') and F(groups__contains='test'))
+    result = cisco.run(napalm_ping, dest="192.168.122.1")
+    print_result(result)
 
 
 if __name__ == "__main__":
